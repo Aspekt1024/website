f92e7b0005a1fa1885fb7bcf4459a50bc32e76dc
Dan <d.szabo87@gmail.com>
2019-02-16 12:16:05 +1100

    Moving Navbar out of Main to save on draw calls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './Components/Header';
 import Main from './Components/Main'
+import Navbar from './Components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Navbar />
         <Main />
       </div>
     );
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,30 +1,23 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
 import Home from './Pages/Home';
 import Projects from './Pages/Projects';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
 import NotFound from './Pages/NotFound';
 import {HashRouter as Router, Switch, Route} from 'react-router-dom'
-import Navbar from './Navbar';
 
 class Main extends Component {
   render() {
     return (
-      <Fragment>
-
-        <Navbar />
-        
-        <Router>
-            <Switch className='body'>
-              <Route exact path='/' component={Home} />
-              <Route path='/projects' component={Projects} />
-              <Route path='/about' component={About} />
-              <Route path='/contact' component={Contact} />
-              <Route component={NotFound} />
-            </Switch>
-        </Router>
-        
-      </Fragment>
+      <Router>
+          <Switch className='body'>
+            <Route exact path='/' component={Home} />
+            <Route path='/projects' component={Projects} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+      </Router>
     )
   }
 }
